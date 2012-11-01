26d9e63c6792fcbe97a91d1de4c9d9083d0242b6
Ryan Murray <ryan@2plik8.com.au>
2012-11-01 16:41:59 +1000

    basic licence function in place

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -7,16 +7,15 @@ class PdfController < ApplicationController
 	def create 
 		u = User.find(params[:user_id])
 		filename = "public/#{params[:document_url]}"
-		pdf = Prawn::Document.generate("#{u.name}.pdf", :template => filename) do
-			go_to_page(1)
-			fill_color "ff0000"
-			text "This pdf has been generated for #{u.name}", :align => :center
+		licence = params[:licence][:licence_ids]
+		licence.each do |l|
+			Prawn::Document.generate("#{l}.pdf", :template => filename) do
+				go_to_page(1)
+				fill_color "ff0000"
+				text "This pdf has been generated for #{l}", :align => :center
+			end
 		end
-		if pdf.nil?
-			redirect_to new
-		else
-			redirect_to u
-		end
+		redirect_to u
 	end
 
 end
